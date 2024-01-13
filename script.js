5d99288a3ab1b6f6bdebf6d7b673e26f7d44860a
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-13 21:21:33 +0530

    added page2 with locomotive image scroll effect

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function videoCon(){
     var videocon = document.querySelector("#video-container")
     var playbtn = document.querySelector("#play")
