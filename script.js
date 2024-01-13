5fcbf6c52eb302a87ba00350db594738cc55de6f
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-13 20:58:45 +0530

    added page1

---
+++ script.js
@@ -0,0 +1,46 @@
+function videoCon(){
+    var videocon = document.querySelector("#video-container")
+    var playbtn = document.querySelector("#play")
+    
+    videocon.addEventListener("mouseenter",function(dets){
+        gsap.to(playbtn,{
+            opacity:1,
+            scale:1
+        })
+    })
+    
+    videocon.addEventListener("mouseleave",function(){
+          gsap.to(playbtn,{
+            opacity:0,
+            scale:0
+        })
+    })
+    
+    videocon.addEventListener("mousemove", function(dets){
+       gsap.to(playbtn, {
+        left:dets.x-90,
+        top:dets.y-90
+       })
+    })
+}
+
+function loadingAnimation(){
+    gsap.from("#page1 h1", {
+        y:100,
+        opacity:0,
+        delay:0.3,
+        duration:0.8,
+        stagger:0.3
+    })
+    gsap.from("#page1 #video-container", {
+        scale:0.9,
+        opacity:0,
+        delay:1.3,
+        duration:0.5,
+    })
+}
+
+loadingAnimation()
+videoCon()
+
+
