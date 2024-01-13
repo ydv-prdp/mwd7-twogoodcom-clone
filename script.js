22a99d7527a821e90204ee485f77c50fa2741c51
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-13 22:19:13 +0530

    added pag3

--- script.js
+++ script.js
@@ -45,6 +45,17 @@ function loadingAnimation(){
     })
 }
 
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor", {
+        left:dets.x,
+        top:dets.y,
+    })
+})
+document.querySelector("#child1").addEventListener("mouseenter",function(){
+    gsap.to("#cursor",{
+        transform:'translate(-50%,-50%) scale(1)'
+    })
+})
 loadingAnimation()
 videoCon()
 
